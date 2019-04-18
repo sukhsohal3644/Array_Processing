ecfc7a696562af173955a17c02bd9104e5f69477
visualstudio <visualstudio@230-PC32>
2019-04-18 14:31:10 -0400

    Process

--- ArrayProcessing/Program.cs
+++ ArrayProcessing/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +10,24 @@ namespace ArrayProcessing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=====");
+       int[] arr = { 5, 10, 9, 3, 6, 11, 2 , 11, 8, 15 };
+            int arr_size = arr.Length;
+
+            printRepeating(arr, arr_size);
         }
+        static void printRepeating(int[] arr, int size)
+        {
+            int i, j;
+
+            Console.WriteLine("Repeated Elements are :");
+            for (i = 0; i < size; i++)
+            {
+                for (j = i + 1; j < size; j++)
+                {
+                    if (arr[i] == arr[j])
+                      Console.WriteLine(arr[i] + " ");
+              }
+          }
+       }
     }
 }

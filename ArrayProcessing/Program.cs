66f4d284fc0a90b13c536ec131d66ee8be293300
sukhsohal3644 <41071247+sukhsohal3644@users.noreply.github.com>
2019-04-19 23:06:12 -0400

    Part2-Longestword

--- ArrayProcessing/Program.cs
+++ ArrayProcessing/Program.cs
@@ -10,24 +10,19 @@ namespace ArrayProcessing
     {
         static void Main(string[] args)
         {
-       int[] arr = { 5, 10, 9, 3, 6, 11, 2 , 11, 8, 15 };
-            int arr_size = arr.Length;
-
-            printRepeating(arr, arr_size);
-        }
-        static void printRepeating(int[] arr, int size)
-        {
-            int i, j;
-
-            Console.WriteLine("Repeated Elements are :");
-            for (i = 0; i < size; i++)
-            {
-                for (j = i + 1; j < size; j++)
-                {
-                    if (arr[i] == arr[j])
-                      Console.WriteLine(arr[i] + " ");
-              }
-          }
-       }
-    }
+			string line = "Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine.";
+			string words = line.split(' ');
+			string word = "";
+			int counter = 0;
+			foreach(string s in words){
+				if(s.Length > counter)
+				{
+					word =s;
+					counter = s.length;
+				}
+			}			
+			Console.WriteLine(word);
+			Console.ReadLine();
+		}
+	}
 }
